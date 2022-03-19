36ae6dc90cde2a5bd79a29abc68f217c479ff834
Ritvik896 <80407600+Ritvik896@users.noreply.github.com>
2022-03-19 21:55:50 +0530

    Created some changes

--- Health_management_syd.py
+++ Health_management_syd.py
@@ -19,6 +19,120 @@ def getDate():
 # """
 
 
+# def getTime():
+#     import datetime
+#     return datetime.datetime.now()
+#
+# def log():
+#     y = int(input("Enter your client: 1)Harry 2)Rohan 3)Hammad 4)Exit: "))
+#     if(y==1):
+#         z = int(input("Enter choice: 1) Exercise 2) Diet 3)Exit:"))
+#         if(z==1):
+#             f =open("Harry_exercise.txt","a")
+#             data1 = input("Enter your exercise: ")
+#             f.write(str(getTime())+" "+data1+"\n")
+#             f.close()
+#             exit()
+#         elif(z==2):
+#             f = open("Harry_diet.txt","a")
+#             data2 = input("Enter your diet: ")
+#             f.write(str(getTime()) + " " + data2 + "\n")
+#             f.close()
+#             exit()
+#         else:
+#             exit()
+#
+#     elif(y == 2):
+#         z = int(input("Enter choice: 1) Exercise 2) Diet 3)Exit:"))
+#         if (z == 1):
+#             f = open("Rohan_exercise.txt", "a")
+#             data3 = input("Enter your exercise: ")
+#             f.write(str(getTime()) + " " + data3 + "\n")
+#             f.close()
+#             exit()
+#         elif (z == 2):
+#             f = open("Rohan_diet.txt", "a")
+#             data4 = input("Enter your diet: ")
+#             f.write(str(getTime()) + " " + data4 + "\n")
+#             f.close()
+#             exit()
+#         else:
+#             exit()
+#     elif (y == 3):
+#         z = int(input("Enter choice: 1) Exercise 2) Diet 3)Exit:"))
+#         if (z == 1):
+#             f = open("Hammad_diet.txt_exercise.txt", "a")
+#             data5 = input("Enter your exercise: ")
+#             f.write(str(getTime()) + " " + data5 + "\n")
+#             f.close()
+#             exit()
+#         elif (z == 2):
+#             f = open("Hammad_diet.txt_diet.txt", "a")
+#             data6 = input("Enter your diet: ")
+#             f.write(str(getTime()) + " " + data6 + "\n")
+#             f.close()
+#             exit()
+#         else:
+#             exit()
+#     else:
+#         exit()
+#
+#
+# def retrieve():
+#     y = int(input("Enter your client: 1)Harry 2)Rohan 3)Hammad  4)Exit: "))
+#     if(y==1):
+#         x = int(input("Enter choice: 1)Exercise 2)Diet 3)exit"))
+#         if(x==1):
+#             f=open("Harry_exercise.txt","r")
+#             print(f.read())
+#         elif(x==2):
+#             f = open("Harry_diet.txt", "r")
+#             print(f.read())
+#         f.close()
+#         exit()
+#     elif (y == 2):
+#         x = int(input("Enter choice: 1)Exercise 2)Diet 3)exit"))
+#         if (x == 1):
+#             f = open("Rohan_exercise.txt", "r")
+#             print(f.read())
+#             f.close()
+#             exit()
+#         elif (x == 2):
+#             f = open("Rohan_diet.txt", "r")
+#             print(f.read())
+#             f.close()
+#             exit()
+#         exit()
+#     elif (y == 3):
+#         x = int(input("Enter choice: 1)Exercise 2)Diet 3)exit"))
+#         if (x == 1):
+#             f = open("Hammad_exercise.txt", "r")
+#             print(f.read())
+#             f.close()
+#             exit()
+#         elif (x == 2):
+#             f = open("Hammad_diet.txt", "r")
+#             print(f.read())
+#             f.close()
+#             exit()
+#         else:
+#             exit()
+#     else:
+#         exit()
+#
+# print("HEALTHCARE SYSTEM")
+# x = int(input("Do you want to: 1)Log  2)Retrieve 3)Exit: "))
+# while(x!=3):
+#     if(x==1):
+#         log()
+#     elif(x==2):
+#         retrieve()
+#     else:
+#         x = int(input("Choose again: "))
+
+
+
+# This is the change
 def getTime():
     import datetime
     return datetime.datetime.now()
@@ -31,16 +145,11 @@ def log():
             f =open("Harry_exercise.txt","a")
             data1 = input("Enter your exercise: ")
             f.write(str(getTime())+" "+data1+"\n")
-            f.close()
-            exit()
         elif(z==2):
             f = open("Harry_diet.txt","a")
             data2 = input("Enter your diet: ")
             f.write(str(getTime()) + " " + data2 + "\n")
-            f.close()
-            exit()
-        else:
-            exit()
+        f.close()
 
     elif(y == 2):
         z = int(input("Enter choice: 1) Exercise 2) Diet 3)Exit:"))
@@ -48,32 +157,22 @@ def log():
             f = open("Rohan_exercise.txt", "a")
             data3 = input("Enter your exercise: ")
             f.write(str(getTime()) + " " + data3 + "\n")
-            f.close()
-            exit()
         elif (z == 2):
             f = open("Rohan_diet.txt", "a")
             data4 = input("Enter your diet: ")
             f.write(str(getTime()) + " " + data4 + "\n")
-            f.close()
-            exit()
-        else:
-            exit()
+        f.close()
     elif (y == 3):
         z = int(input("Enter choice: 1) Exercise 2) Diet 3)Exit:"))
         if (z == 1):
             f = open("Hammad_diet.txt_exercise.txt", "a")
             data5 = input("Enter your exercise: ")
             f.write(str(getTime()) + " " + data5 + "\n")
-            f.close()
-            exit()
         elif (z == 2):
             f = open("Hammad_diet.txt_diet.txt", "a")
             data6 = input("Enter your diet: ")
             f.write(str(getTime()) + " " + data6 + "\n")
-            f.close()
-            exit()
-        else:
-            exit()
+        f.close()
     else:
         exit()
 
@@ -85,43 +184,29 @@ def retrieve():
         if(x==1):
             f=open("Harry_exercise.txt","r")
             print(f.read())
-            f.close()
-            exit()
         elif(x==2):
             f = open("Harry_diet.txt", "r")
             print(f.read())
-            f.close()
-            exit()
-        else:
-            exit()
+        f.close()
     elif (y == 2):
         x = int(input("Enter choice: 1)Exercise 2)Diet 3)exit"))
         if (x == 1):
             f = open("Rohan_exercise.txt", "r")
             print(f.read())
-            f.close()
-            exit()
         elif (x == 2):
             f = open("Rohan_diet.txt", "r")
             print(f.read())
-            f.close()
-            exit()
-        else:
-            exit()
+        f.close()
     elif (y == 3):
         x = int(input("Enter choice: 1)Exercise 2)Diet 3)exit"))
         if (x == 1):
             f = open("Hammad_exercise.txt", "r")
             print(f.read())
-            f.close()
-            exit()
+
         elif (x == 2):
             f = open("Hammad_diet.txt", "r")
             print(f.read())
-            f.close()
-            exit()
-        else:
-            exit()
+        f.close()
     else:
         exit()
 
@@ -130,14 +215,12 @@ x = int(input("Do you want to: 1)Log  2)Retrieve 3)Exit: "))
 while(x!=3):
     if(x==1):
         log()
+        x = int(input("Choose again: 1)Log  2)Retrieve 3)Exit:"))
     elif(x==2):
         retrieve()
-    else:
-        x = int(input("Choose again: "))
-
-
-
-# This is the change
+        x = int(input("Choose again: 1)Log  2)Retrieve 3)Exit:"))
+    elif(x==3):
+        break
 
 
 
